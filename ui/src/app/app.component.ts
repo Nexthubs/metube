a8224801e3b2c1dda1cb149dc735d6c456ee0128
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-05-06 19:01:12 +0800

    Update app.component.ts

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -61,7 +61,7 @@ export class AppComponent implements AfterViewInit {
   faExternalLinkAlt = faExternalLinkAlt;
 
   constructor(public downloads: DownloadsService, private cookieService: CookieService, private http: HttpClient) {
-    this.format = cookieService.get('metube_format') || 'any';
+    this.format = cookieService.get('metube_format') || 'mp4';
     // Needs to be set or qualities won't automatically be set
     this.setQualities()
     this.quality = cookieService.get('metube_quality') || 'best';
