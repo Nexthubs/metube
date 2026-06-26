d0fef5f91572ae4194bf7f927a097444973f5ea0
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-06-26 17:07:47 +0800

    Update formats.ts

--- ui/src/app/interfaces/formats.ts
+++ ui/src/app/interfaces/formats.ts
@@ -17,7 +17,7 @@ export const DOWNLOAD_TYPES: Option[] = [
 ];
 
 export const VIDEO_CODECS: Option[] = [
-  { id: "auto", text: "Auto" },
+  { id: "auto", text: "自动" },
   { id: "h264", text: "H.264" },
   { id: "h265", text: "H.265 (HEVC)" },
   { id: "av1", text: "AV1" },
@@ -25,13 +25,13 @@ export const VIDEO_CODECS: Option[] = [
 ];
 
 export const VIDEO_FORMATS: Option[] = [
-  { id: "any", text: "Auto" },
+  { id: "any", text: "自动" },
   { id: "mp4", text: "MP4" },
-  { id: "ios", text: "iOS Compatible" },
+  { id: "ios", text: "iOS 兼容" },
 ];
 
 export const VIDEO_QUALITIES: Quality[] = [
-  { id: "best", text: "Best" },
+  { id: "best", text: "最佳" },
   { id: "2160", text: "2160p" },
   { id: "1440", text: "1440p" },
   { id: "1080", text: "1080p" },
@@ -39,7 +39,7 @@ export const VIDEO_QUALITIES: Quality[] = [
   { id: "480", text: "480p" },
   { id: "360", text: "360p" },
   { id: "240", text: "240p" },
-  { id: "worst", text: "Worst" },
+  { id: "worst", text: "最差" },
 ];
 
 export const AUDIO_FORMATS: AudioFormatOption[] = [
@@ -47,7 +47,7 @@ export const AUDIO_FORMATS: AudioFormatOption[] = [
     id: "m4a",
     text: "M4A",
     qualities: [
-      { id: "best", text: "Best" },
+      { id: "best", text: "最佳" },
       { id: "192", text: "192 kbps" },
       { id: "128", text: "128 kbps" },
     ],
@@ -56,15 +56,15 @@ export const AUDIO_FORMATS: AudioFormatOption[] = [
     id: "mp3",
     text: "MP3",
     qualities: [
-      { id: "best", text: "Best" },
+      { id: "best", text: "最佳" },
       { id: "320", text: "320 kbps" },
       { id: "192", text: "192 kbps" },
       { id: "128", text: "128 kbps" },
     ],
   },
-  { id: "opus", text: "OPUS", qualities: [{ id: "best", text: "Best" }] },
-  { id: "wav", text: "WAV", qualities: [{ id: "best", text: "Best" }] },
-  { id: "flac", text: "FLAC", qualities: [{ id: "best", text: "Best" }] },
+  { id: "opus", text: "OPUS", qualities: [{ id: "best", text: "最佳" }] },
+  { id: "wav", text: "WAV", qualities: [{ id: "best", text: "最佳" }] },
+  { id: "flac", text: "FLAC", qualities: [{ id: "best", text: "最佳" }] },
 ];
 
 export const CAPTION_FORMATS: Option[] = [
